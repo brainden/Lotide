6fb01b1406d2d304e0a9a71c13376580ccb2d177
Brandon <brandonkilic@gmail.com>
2019-06-27 03:08:39 +0000

    letterPosition

--- letterPositions.js
+++ letterPositions.js
@@ -1,25 +1,22 @@
-const assertArraysEqual = function(arr1, arr2) {
-    if (eqArrays(arr1, arr2)) {
-      console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}`);
-    } else {
-      console.log(`🔴🔴🔴Assertion Failed: ${arr1} !== ${arr2}`);
-    }
-  };
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const letterPositions = function(sentence) {
+ 
+ let result = {};
+ let strSplit = sentence.split('')
+ for(let i= 0 ; i< strSplit.length; i++){
+   
+   result[strSplit[i]] =  [i];
   
-  function eqArrays(arr1,arr2){
-    if(arr1.length !== arr2.length){
-        return false;
-    }
-    for (let i = 0 ; i < arr1.length; i++){
-            if(arr1[i] !== arr2[i]){
-                return false;
-            }
-          } return true;
-        }
+   }return result;
+ }
 
 
-
-
-
-
-assertArraysEqual(eqArrays("hello"));
+console.log(letterPositions("hello"));
