d00a2a8feeaa42c9999a991aa84061281eb92edd
Brandon <brandonkilic@gmail.com>
2019-06-29 00:26:31 +0000

    June 28, 8:36pm

--- tail.js
+++ tail.js
@@ -1,20 +1,16 @@
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-    } else if (actual !== expected) {
-      console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const tail1 = function(arr2) {
+  for (let i = 0; i < arr2.length; i++) {
+    if (i !== 0) {
+      console.log(arr2[i]);
     }
-  };
-function tail1(arr2){ 
-
-for(let i=0; i < arr2.length; i++){
-  let newArr = [];
-  if(arr2[i] !== arr2[0]){
-    newArr.push(arr2[i]);
-  } 
-  
-console.log(newArr);
-}
-}
+  }
+};
 
 console.log(tail1([1,2,3,4,5,6]));
